e2b0fe805474f271cdc94e5086fb516d3c32d19d
Zeeshan Iqbal <link2zeshan@gmail.com>
2019-07-06 04:56:29 +0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "@ionic/core/css/core.css";
+import "@ionic/core/css/ionic.bundle.css";
+import {
+  IonApp,
+  IonContent,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle
+} from "@ionic/react";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <IonApp>
+        <IonContent>
+          <IonCard>
+            <IonCardHeader>
+              <IonCardSubtitle>Welcome to Ionic</IonCardSubtitle>
+              <IonCardTitle>Running on React</IonCardTitle>
+            </IonCardHeader>
+          </IonCard>
+        </IonContent>
+      </IonApp>
+    );
+  }
 }
 
 export default App;
